74210016ab75df46235670d63f66c643d5477f09
Thalys Gama <86577400+ThalysGama13@users.noreply.github.com>
2021-09-07 17:24:49 -0300

    Delete triangulopascal.py

--- triangulopascal.py
+++
@@ -1,9 +0,0 @@
-from math import factorial
-n = int(input('Digite um num '))
-
-triângulo = list()
-for c in range(0,n):
-    for j in range(0,n+c-1):
-        comb=factorial(c)/(factorial(j)*factorial(j-c))
-        triângulo.append(comb)
-    print(comb)
